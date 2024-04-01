ed7515022fd297e21063a7631468b9d72cb07712
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2024-04-01 13:57:59 +0100

    Created a new file for sending mails through nodemailer

--- Database/connectDB.js
+++ Database/connectDB.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
 
 const connectDB = async (url) => {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(url, {});
+  try {
+    await mongoose.connect(url);
 
-    // Event listeners for connection-related events
     mongoose.connection.on("connected", () => {
       console.log("Mongodb Atlas Database Connected...");
-      resolve();
     });
 
     mongoose.connection.on("error", (err) => {
       console.error(`MongoDB connection error: ${err}`);
-      reject(err);
     });
-  });
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error}`);
+  }
 };
 
 module.exports = connectDB;
