4829588b483b5b79868a8dfc7d6978e854f2da72
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2024-04-19 13:41:41 +0100

    Images are now being geerated by prompt using unsplash API

--- Routes/thirdPartiesRoutes.js
+++ Routes/thirdPartiesRoutes.js
@@ -4,8 +4,10 @@ const {
   geminiChatHeroSectionHeader,
   geminiChatHeroSectionDescription,
 } = require("../Controller/geminiController");
+const searchImages = require("../Controller/unsplashController");
 
 router.post("/gemini-chat-hero-section-header", geminiChatHeroSectionHeader);
 router.post("/gemini-chat-hero-section-description", geminiChatHeroSectionDescription);
+router.post("/search-image", searchImages);
 
 module.exports = router
