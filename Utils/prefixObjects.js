2c9afebe6950f3acff4f1a2fed55c15a72c2bd36
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2024-04-23 18:31:36 +0100

    Added a new line of code that generates company names

--- Utils/prefixObjects.js
+++ Utils/prefixObjects.js
@@ -1,4 +1,6 @@
 const prefixForPrompts = {
+  promptPrefixLogo:
+    "In just one word describe a good company name for",
   promptPrefixForHeroHeader:
     "Write a short header not less than 3 words and not more than 4 words without any special characters it must be 4 words for a hero section for",
   promptPrefixForHeroDescription:
